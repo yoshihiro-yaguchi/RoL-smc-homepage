9c5dc4d94abd70b2c682d6a527535ea67560636e
yaguchi <yoshihiro19990307@outlook.com>
2022-05-22 18:30:32 +0900

    画面幅を1440pxに変更

--- front/src/App.tsx
+++ front/src/App.tsx
@@ -1,9 +1,6 @@
 import React from 'react'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import { Header } from 'features/header/Header'
-import { Pzzhg000 } from './features/pzzhg000/Pzzhg000'
-import { Pzzhg001 } from './features/pzzhg001/Pzzhg001'
-import { Pzzhg002 } from './features/pzzhg002/Pzzhg002'
 import { Box } from '@mui/material'
 import { About } from 'features/about/about'
 import { Business } from 'features/business/business'
@@ -14,7 +11,7 @@ import { RouteIndex } from 'features/routeIndex/routeIndex'
 export const App = () => {
   return (
     <BrowserRouter>
-      <Box sx={{ width: '1024px', margin: '0 auto' }}>
+      <Box sx={{ width: '1440px', margin: '0 auto' }}>
         <Header />
         <Routes>
           <Route index element={<RouteIndex />} />
