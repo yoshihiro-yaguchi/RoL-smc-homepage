ca9d0e86dd07b00bcba2757dd9d168c57faad6af
DESKTOP-21F96V4\yoshihiro-yaguchi <yoshihiro19990307@outlook.com>
2022-05-30 03:06:07 +0900

    ブログのイメージを追加

--- front/src/features/blog/blogSummary/blogSummary.tsx
+++ front/src/features/blog/blogSummary/blogSummary.tsx
@@ -6,6 +6,8 @@ import { ScreenBox, ViewDetailInfoBox } from 'common/commonComponents'
 import { Box, Typography } from '@mui/material'
 import { Cell } from './components/Cell'
 import { useNavigate } from 'react-router-dom'
+import BlogImage from 'pic/blog/abstract-g9de7c00c2_1920.jpg'
+import { common } from '@mui/material/colors'
 
 export const BlogSummary = () => {
   const states = useAppSelector((state: RootState) => state.blogSummary)
@@ -22,6 +24,25 @@ export const BlogSummary = () => {
 
   return (
     <>
+      <Box
+        sx={{
+          width: '100%',
+          marginTop: '100px',
+        }}
+      >
+        <img
+          src={BlogImage}
+          alt="BusinessImage"
+          style={{ width: '100%', height: '500px', objectFit: 'none', borderRadius: '10px' }}
+        />
+        <Typography
+          color={common['white']}
+          fontSize={100}
+          sx={{ position: 'absolute', top: '50%', left: '50%', transform: 'translate(-50%, -70%)' }}
+        >
+          <strong>Blog.</strong>
+        </Typography>
+      </Box>
       <ScreenBox>
         <Box>
           <Typography fontSize={25}>
